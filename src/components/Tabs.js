d14a4e3465954403d12f2e65432fc3dcd2f987a2
Jamil-4khtar <jamil4khtar@gmail.com>
2024-12-05 04:35:08 +0530

    created a submit

---
+++ src/components/Tabs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Tabs() {
+    return (
+        <div className='tabs'>
+            <div className='tab' id='Home-Tab'>Home</div>
+            <div className='tab' id='About-Tab'>About</div>
+            <div className='tab' id='Features-Tab'>Features</div>
+        </div>
+    )
+}
+
+export default Tabs
---
+++ src/components/Viewport.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Viewport() {
+    return (
+        <div className='viewport'>
+            Pages Go Here
+        </div>
+    )
+}
+
+export default Viewport

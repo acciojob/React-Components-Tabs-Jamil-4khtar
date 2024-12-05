6875abb7ba3e24e7a2e522352661868f6bfc0ed4
Jamil-4khtar <jamil4khtar@gmail.com>
2024-12-05 15:48:49 +0530

    ok

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Tabs() {
     return (
         <div className='tabs'>
-            <div id='Home-tab' className='tab'>Home</div>
-            <div id='About-tab' className='tab'>About</div>
-            <div id='Features-tab' className='tab'>Features</div>
+            <div id='Home-Tab' className='tab'>Home</div>
+            <div id='About-Tab' className='tab'>About</div>
+            <div id='Features-Tab' className='tab'>Features</div>
 
         </div>
     )
